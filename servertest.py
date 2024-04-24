7e2c34166f67c3ae802c4061ea8483c03b638feb
我沒code 我沒code <135327579+ZouBadCode@users.noreply.github.com>
2024-04-25 00:16:08 +0800

    Add files via upload

---
+++ servertest.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+html_content = "Edit this content..."
+# 添加變數存儲div的位置
+div_position = {'x': 50, 'y': 50}  # 初始位置
+
+@app.route('/insert')
+def insert_page():
+    return render_template('test_insert.html')
+@app.route('/')
+def index():
+    return render_template('workspace.html')
+
+@socketio.on('update content')
+def handle_content(data):
+    global html_content
+    html_content = data
+    emit('update content', data, broadcast=True)
+
+@socketio.on('update position')
+def handle_position(data):
+    global div_position
+    div_position = data
+    emit('update position', data, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
